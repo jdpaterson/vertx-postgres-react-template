2c3c0cfa14af8c0d367c872c55e82b126465dc60
Jeremy Paterson <jeremy.paterson@globalrelay.net>
2022-07-12 14:49:59 -0700

    WIP get intellij working with react

--- src/main/java/tupper_soccer/tupper_soccer/MainVerticle.java
+++ src/main/java/tupper_soccer/tupper_soccer/MainVerticle.java
@@ -2,16 +2,21 @@ package tupper_soccer.tupper_soccer;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
+    Router router = Router.router(vertx);
+    Route messageRoute = router.get("/api/message");
+    messageRoute.handler(rc -> {
+      rc.response().end("Hello from backend!");
+    });
+    router.get().handler(StaticHandler.create());
+    vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
